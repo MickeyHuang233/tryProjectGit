4f1a6fa40204dca154a797caaf82e0a3d835284d
Mickey <MickeyProjectMail233@gmail.com>
2018-08-03 12:11:10 +0800

    [JSF]增加必填欄位、數字范圍和文字長度的驗證

---
+++ tryJSF/src/com/mickey/Student.java
@@ -0,0 +1,108 @@
+package com.mickey;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String country;
+	private String favoriteLanguage;
+	private String[] hobby;
+	List<String> countryOptions;
+	List<String> favoriteLanguageOptions;
+	List<String> hobbyOptions;
+	
+	public Student() {
+		//countryﬂx??
+		countryOptions = new ArrayList<String>();
+		countryOptions.add("China");
+		countryOptions.add("USA");
+		countryOptions.add("UK");
+		countryOptions.add("Brazil");
+		countryOptions.add("France");
+		countryOptions.add("SoundHorizon");
+		countryOptions.add("Japan");
+		
+		//favoriteLanguageﬂx??
+		favoriteLanguageOptions = new ArrayList<String>();
+		favoriteLanguageOptions.add("Java");
+		favoriteLanguageOptions.add("C#");
+		favoriteLanguageOptions.add("Ruby");
+		favoriteLanguageOptions.add("PHP");
+		
+		//hobbyﬂx??
+		hobbyOptions = new ArrayList<String>();
+		hobbyOptions.add("playBall");
+		hobbyOptions.add("study");
+		hobbyOptions.add("watchTV");
+		hobbyOptions.add("computer");
+		
+		//?ì√ÊÀ¢–¬ƒ¨’Jﬂx??
+		this.firstName = "USAKi";
+		country = "Japan";
+		favoriteLanguage = "Java";
+		String[] temphobby = {"playBall", "computer"};
+		hobby = temphobby;
+		
+	}
+	
+	public List<String> getCountryOptions() {
+		return countryOptions;
+	}
+	
+	public List<String> getFavoriteLanguageOptions() {
+		return favoriteLanguageOptions;
+	}
+	
+	public List<String> getHobbyOptions() {
+		return hobbyOptions;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+	
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+	
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+
+	public String[] getHobby() {
+		return hobby;
+	}
+
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+
+	
+	
+}
